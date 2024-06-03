052ac6660defea1b549bdf9cb7ce6227ae6b9e36
Nacky <N4cky24@gmail.com>
2024-06-03 18:29:17 +0330

    New Commit

--- fundamentals/src/app/app.module.ts
+++ fundamentals/src/app/app.module.ts
@@ -4,18 +4,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BasicComponent} from "./basic.component";
-import { WowComponent } from './wow/wow.component';
 import {NamesService} from "./names.service";
+import { FaviconComponent } from './favicon/favicon.component';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicComponent,
-    WowComponent
+    FaviconComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration(),
--- fundamentals/src/app/wow/wow.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-wow',
-  templateUrl: './wow.component.html',
-  styleUrl: './wow.component.css'
-})
-export class WowComponent {
-}
